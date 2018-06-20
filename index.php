4c7396f292cfe74f2f220466a3d915dae4c25aca
Alfredo Marcano <alfredomarcan@gmail.com>
2018-06-19 22:44:05 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+$conexion = mysqli_connect('localhost', 'root', '', 'entrevista');
+// if ($conexion = !1) {
+//     echo "error en la conexion por favor verificar su usuario y contraseña";
+// } else {
+//     echo "conexion con exito";
+// }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+   <header><span class="fas fa-bars show"></span></header>
+    <main>
+        <div class="content-menu">
+
+            <li>
+                <a href="inicio.php" target="frame">
+                    <i class="fas fa-home icon1"></i>
+                    <h4 class="text1">Inicio</h4>
+                </a>
+            </li>
+            <li>
+                <a href="admin/equipos/index.php" target="frame">
+                    <i class="fas fa-desktop icon2"></i>&nbsp<i class="fas fa-laptop icon3"></i>
+                    <h4 class="text2">Equipos Operativos</h4>
+                </a>
+            </li>
+            <li>
+                    <i class="fas fa-warehouse icon4"></i>
+                    <h4 class="text4">Almacen</h4>
+            </li>
+            <li>
+                <a href="admin/mantenimiento/index.php" target="frame">
+                    <i class="fas fa-cogs icon5"></i>
+                    <h4 class="text5">Mantenimiento</h4>
+                </a>
+            </li>
+            <li>
+                    <i class="far fa-arrow-alt-circle-down icon6"></i>
+                    <h4 class="text6">Desincorporar</h4>
+            </li>
+            <li>
+                    <i class="far fa-file-pdf icon7"></i>
+                    <h4 class="text7">Reportes</h4>
+            </li>
+            <li>
+                    <i class="far fa-envelope icon8"></i>
+                    <h4 class="text7">Mensajes</h4>
+            </li>
+            <li><i class="fas fa-sign-out-alt icon9"></i><h4 class="text8">Sesion</h4></li>
+            <!-- <li><i class="fa fa-question-circle icon9"></i><h4 class="text9">Nosotros</h4></li> -->
+
+        </div>
+        <article>
+            <iframe name="frame" id="frame" class="ventana" src="inicio.php" width="100%" height="830px" frameborder="0" scrolling="no"></iframe>
+        </article>
+    </main>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ inicio.php
@@ -0,0 +1,41 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<article>
+	<h1>Sistema de Inventarios</h1>
+
+            <ul class="flex-container">
+                <li class="flex-item menu1">
+                    <i class="fas fa-desktop icon1"></i>
+                    <p>Escritorio</p>
+                </li>
+                <li class="flex-item menu2">
+                    <i class="fas fa-laptop"></i>
+                    <p>Laptos</p>
+                </li>
+                <li class="flex-item menu3">
+                    <i class="fas fa-ambulance"></i>
+                    <p>Mantenimeinto</p>
+                </li>
+                <li class="flex-item menu4">
+                    <i class="fas fa-warehouse"></i>
+                    <p>Almacen</p>
+                </li>
+                <li class="flex-item">
+                    <i class="fas fa-chart-pie"></i>
+                    <p>Balance</p>
+                </li>
+            </ul>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa recusandae enim quo suscipit quos, totam, exercitationem facere iusto eaque at voluptate quam quaerat aut illum laborum unde distinctio odit, tempore, voluptas. Molestiae magnam expedita sint ullam commodi dignissimos asperiores optio pariatur, ab architecto dolores impedit odit laboriosam fugiat praesentium deleniti illo nemo a temporibus consectetur! Exercitationem natus ducimus accusantium, iusto incidunt quo id illum harum possimus accusamus omnis, voluptate reiciendis tenetur, fugit repudiandae deleniti alias quos dolore numquam. Rerum molestiae blanditiis, repudiandae enim amet suscipit eius expedita iste vel, ab cum consectetur, voluptate temporibus minima delectus tempora doloremque voluptas doloribus nesciunt ipsum ipsa esse. Provident illo iusto ullam molestiae,  voluptatibus possimus rem, autem laborum maxime quae, itaque reiciendis unde cupiditate perferendis sunt? Porro voluptatem ea consequuntur, rem velit totam reprehenderit, perferendis nemo expedita aliquid, laborum dolores. At temporibus voluptate aliquid voluptas ipsa? Rem amet recusandae illum cum at ipsum, beatae porro ea, vel adipisci dolorem sunt optio ut nobis minima rerum reprehenderit explicabo consectetur aliquam assumenda magni quis! Modi aspernatur atque possimus magnam obcaecati perspiciatis ratione inventore iure repellendus, quas, expedita soluta alias architecto vero dolores.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nesciunt vel vitae praesentium, omnis quas obcaecati provident ab, corporis quos labore illo eum odit cum eligendi modi maiores, neque voluptatibus.</p>
+            </article>
+</body>
+</html>
